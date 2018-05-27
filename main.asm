609af24668d495fe13d17dd29643b03bf0e08989
copsicle <gonen.cohen43@gmail.com>
2018-05-27 14:55:42 +0300

    Added outragous comments

--- main.asm
+++ main.asm
@@ -1,20 +1,34 @@
 
 INCLUDE \masm32\include\masm32rt.inc
 
+;Include essential masm stuff
+
 INCLUDE mainFuncs.inc
+
+;Include all of the functions
 
 .386
 
+;Limit CPU instructions that masm is allwed to use
+
 .MODEL flat, STDCALL
+
+;Define memory model to use, doesn't seem to work rn
 
 .CODE
 	
 	main PROC
 
+	;Start of main function
+
 		INVOKE init
 
+		;Call the initialize function
+
 		loopa:
-			
+
+		;Start loop 
+
 			INVOKE draw
 
 			INVOKE update
@@ -22,6 +36,8 @@ INCLUDE mainFuncs.inc
 			INVOKE bgcmp
 
 			INVOKE jeffcmp
+
+			;Call all  drawing and updating functions and repeat them infinitly
 
 		JMP loopa
 
